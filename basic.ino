2b7a87be3fa1021b3c7156c59b0d55e8a3f0c420
JrManit <7099505+JrManit@users.noreply.github.com>
2017-11-13 19:14:55 +0100

    Add files via upload

--- basic.ino
+++ basic.ino
@@ -19,33 +19,33 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   // read the current RDAC1 value
-  int ret = digit_pot.read_data(AD5144_ADDR_RDAC1, AD5144_RDAC);
+  int ret = digital_pot.read_data(AD5144_ADDR_RDAC1, AD5144_RDAC);
   Serial.print("Read RDAC1 Value: ");
   Serial.println(ret, DEC);
 
   // set new value to RDAC1 (0~255)
   uint16_t data = 0x123;
-  ret = digit_pot.write_data(AD5144_CTRL_RDAC_WRITE, AD5144_ADDR_RDAC1, data);
+  ret = digital_pot.write_data(AD5144_CTRL_RDAC_WRITE, AD5144_ADDR_RDAC1, data);
   if(ret != 0)  // check if data is sent successfully
     Serial.println("Error!");
     
   // read the new RDAC value
-  ret = digit_pot.read_data(AD5144_ADDR_RDAC1, AD5144_RDAC);
+  ret = digital_pot.read_data(AD5144_ADDR_RDAC1, AD5144_RDAC);
   Serial.print("New RDAC1 Value: ");
   Serial.println(ret, DEC);   
 
   // read the current EEPROM1 value
-  ret = digit_pot.read_data(AD5144_ADDR_RDAC1, AD5144_EEPROM);
+  ret = digital_pot.read_data(AD5144_ADDR_RDAC1, AD5144_EEPROM);
   Serial.print("Read EEPROM1 Value: ");
   Serial.println(ret, DEC);
    
   // copy RDAC1 to EEPROM1
-  ret = digit_pot.copy_data(AD5144_ADDR_RDAC1, AD5144_RDAC2EEPROM);
+  ret = digital_pot.copy_data(AD5144_ADDR_RDAC1, AD5144_RDAC2EEPROM);
   if(ret != 0)    // check if data is sent successfully
     Serial.println("Error!");
   
   // read the new EEPROM1 value
-  ret = digit_pot.read_data(AD5144_ADDR_RDAC1, AD5144_EEPROM);
+  ret = digital_pot.read_data(AD5144_ADDR_RDAC1, AD5144_EEPROM);
   Serial.print("Read EEPROM1 Value: ");
   Serial.println(ret, DEC);
 }
